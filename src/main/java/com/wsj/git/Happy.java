f90e0dd710ca1f9a49fb5501a0de369296fe5306
swenzha_glb <chnwangsijia@163.com>
2020-03-17 21:58:14 +0800

    edit on branch hot_fix

--- src/main/java/com/wsj/git/Happy.java
+++ src/main/java/com/wsj/git/Happy.java
@@ -8,6 +8,6 @@ public static void main(String[] args) {
 	System.out.println("left ... ... ?");
 	System.out.println("left ... ... ?");
 	System.out.println("left ... ... ?");
-	
+	System.out.println("hot fix  ... ... ?");
 }
 }
